0ed2b0f387b3347b4bd1293d0e494b37277238c2
Tamas Bayer <pititamas@gmail.com>
2020-11-16 11:20:01 +0100

    set Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Header} from './components/Header';
-import {Watchlis} from './components/Watchlist';
+import {Watchlist} from './components/Watchlist';
 import {Watched} from './components/Watched';
 import {Add} from './components/Add';
 import './App.css';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Router>
+      <Header/>
+
+      <Switch>
+        <Route exact path="/">
+          <Watchlist/>
+        </Route>
+
+        <Route path="/watched">
+          <Watched/>
+        </Route>
+
+        <Route path="/add">
+          <Add/>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
