a0d64de286a01d2d1bf7f8279b5fb6cb10a07945
Tamas Bayer <pititamas@gmail.com>
2020-11-16 10:51:52 +0100

    import components to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Header} from './components/Header';
+import {Watchlis} from './components/Watchlist';
+import {Watched} from './components/Watched';
+import {Add} from './components/Add';
 import './App.css';
 
 function App() {
